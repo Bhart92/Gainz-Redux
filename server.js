08036baacb020c9223d9a4dc876d8ac4209b2bf9
Brandon H <hartwebdev92@gmail.com>
2021-03-10 13:10:40 -0800

    uninstall packages

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
-  
     next();
   });
 
